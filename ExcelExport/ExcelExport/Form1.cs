cb378aee3096400710466f8025f311c628427e04
Katerina Szabó <katarina.szabo@stud.uni-corvinus.hu>
2021-10-08 14:05:17 +0200

    rossz változtatás

---
+++ ExcelExport/ExcelExport/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel= Microsoft.Office.Interop.Excel;
+
+namespace ExcelExport
+{
+    public partial class Form1 : Form
+    {
+        
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+        }
+    }
+}
