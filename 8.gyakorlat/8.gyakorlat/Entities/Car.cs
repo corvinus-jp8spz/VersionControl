e3d12ca2b8c51dcf86caaa9c34b0406ee5e135be
Katerina Szabó <katarina.szabo@stud.uni-corvinus.hu>
2021-11-12 16:18:47 +0100

    Create Car class

---
+++ 8.gyakorlat/8.gyakorlat/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.gyakorlat.Abstractions
+{
+    public abstract class Car: Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
