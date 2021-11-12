73171141ae358054472c9913106b54a29a80c856
Katerina Szabó <katarina.szabo@stud.uni-corvinus.hu>
2021-11-12 16:39:05 +0100

    Car and Ball button

--- 8.gyakorlat/8.gyakorlat/Form1.Designer.cs
+++ 8.gyakorlat/8.gyakorlat/Form1.Designer.cs
@@ -33,6 +33,10 @@ namespace _8.gyakorlat
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.buttonCar = new System.Windows.Forms.Button();
+            this.buttonBall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // createTimer
@@ -52,10 +56,42 @@ namespace _8.gyakorlat
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.buttonBall);
+            this.mainPanel.Controls.Add(this.buttonCar);
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(766, 434);
             this.mainPanel.TabIndex = 0;
+            // 
+            // buttonCar
+            // 
+            this.buttonCar.Location = new System.Drawing.Point(208, 118);
+            this.buttonCar.Name = "buttonCar";
+            this.buttonCar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCar.TabIndex = 0;
+            this.buttonCar.Text = "CAR";
+            this.buttonCar.UseVisualStyleBackColor = true;
+            this.buttonCar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonBall
+            // 
+            this.buttonBall.Location = new System.Drawing.Point(313, 118);
+            this.buttonBall.Name = "buttonBall";
+            this.buttonBall.Size = new System.Drawing.Size(75, 23);
+            this.buttonBall.TabIndex = 1;
+            this.buttonBall.Text = "BALL";
+            this.buttonBall.UseVisualStyleBackColor = true;
+            this.buttonBall.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next:";
             // 
             // Form1
             // 
@@ -65,6 +101,8 @@ namespace _8.gyakorlat
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -73,6 +111,9 @@ namespace _8.gyakorlat
         private System.Windows.Forms.Timer conveyorTimer;
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBall;
+        private System.Windows.Forms.Button buttonCar;
     }
 }
 
