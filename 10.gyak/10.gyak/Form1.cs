180546829eb982312a1e47ef4ed9893b60c5d8f2
Katerina Szabó <katarina.szabo@stud.uni-corvinus.hu>
2021-12-03 12:40:16 +0100

    Szimuláció

--- 10.gyak/10.gyak/Form1.cs
+++ 10.gyak/10.gyak/Form1.cs
@@ -14,6 +14,8 @@ namespace _10.gyak
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
         List<Person> Population = null;
         List<BirthProbability> BirthProbabilities = null;
         List<DeathProbability> DeathProbabilities = null;
@@ -24,8 +26,24 @@ namespace _10.gyak
             Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
             BirthProbabilities = GetBirthProbailities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    //...
+                }
+                int nbrOfMales= (from x in Population
+                                where x.Gender==Gender.Male && x.IsAlive
+                                select x).Count();
+                int nbrOfFemales = (from x in Population
+                                  where x.Gender == Gender.Female && x.IsAlive
+                                  select x).Count();
+                Console.WriteLine(string.Format(
+                    "Év:{0}\nFiúk:{1}\nLányok:{2}\n",
+                    year, nbrOfMales, nbrOfFemales));
 
 
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
